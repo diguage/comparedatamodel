de363a6dab3647db778127d507d4ac2c7559d589
diguage <leejun119@gmail.com>
2012-12-16 09:06:19 +0800

    调整脚步功能说明和作者声明的格式

--- src/read_config.tcl
+++ src/read_config.tcl
@@ -1,8 +1,11 @@
 # **  此脚本用来读取config.txt配置文件，并将相应信息保存到cfg全局数组里
-# **  创建日期：2010-8-16
+#
 # **  创建人员：付骋
-# **  修改日期：李君（D瓜哥，http://www.diguage.com/）
-# **  修改人员：2012-3-19
+# **  创建日期：2010-8-16
+# 
+# **  修改人员：李君（Nickname：D瓜哥；Website：http://www.diguage.com/）
+# **  修改日期：2012-3-19
+
 global cfg
 set fid [open "$absoluteParentPath//src//config.txt" r]
 while {[eof  $fid] != 1} {
